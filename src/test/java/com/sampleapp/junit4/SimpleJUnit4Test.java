b7bee60aacebea75e92830253ff6eb618ca29967
Matei Dragu <matei.dragu@gmail.com>
2018-03-04 18:52:35 +0000

    JUnit 4 test - use StackWalker to dump the current call stack

--- src/test/java/com/sampleapp/junit4/SimpleJUnit4Test.java
+++ src/test/java/com/sampleapp/junit4/SimpleJUnit4Test.java
@@ -11,6 +11,9 @@ public class SimpleJUnit4Test {
     @Test
     public void junit4TestShouldRunProperly() {
         log.info("This is a JUnit4 test which will be run by the new JUnit Vintage runner");
+        StackWalker.getInstance().forEach(stackFrame -> {
+            log.info("Frame: {}", stackFrame);
+        });
 
         assertThat(1).isGreaterThan(0);
     }
